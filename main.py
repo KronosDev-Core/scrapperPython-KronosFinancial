4d2d81a81c9d5381fedad5efbbf864bf80e47179
KronosDev-Pro <kronosdev.pro@gmail.com>
2022-12-21 02:07:59 +0100

    Update to V1.1.1
     - Adaptation of the frontend due to the new backend
     - other improvements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pymongo
+import asyncio
+from typing import Any, Coroutine
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
@@ -7,6 +8,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from datetime import datetime
 from re import search
+from prisma import Prisma
+from prisma.enums import Status
+from prisma.models import Dividende, Stock
 
 date = datetime.now().strftime("%d-%m-%Y")
 
@@ -16,19 +20,11 @@ option.add_argument("--disable-gpu")
 option.add_argument("--log-level=1")
 option.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-conn_str = "mongodb://localhost:27017/"
-client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
 
-try:
-    client.server_info()
-except Exception:
-    print("Unable to connect to the server.")
+async def main():
+    db = Prisma(auto_register=True)
+    await db.connect()
 
-db = client['KronosFinancial']
-dividende = db['dividendes']
-
-
-def main():
     pathExe = ChromeDriverManager(path=r".\\Drivers").install()
     with webdriver.Chrome(service=ChromeService(pathExe), options=option) as driver:
         driver.get("https://www.etoro.com/fr/investing/dividend-calendar/")
@@ -38,59 +34,130 @@ def main():
         driver.implicitly_wait(5)
         driver.find_element(By.XPATH, '//*[@id="ec-load-more"]').click()
         driver.implicitly_wait(5)
-        for e in driver.find_elements(By.XPATH, '//*[@id="ec-reports-table"]/tbody/tr'):
-            val = e.text.split("\n")
 
-            if (val != [""]):
-                data = {
-                    "Symbol": val[0],
-                    "Date_ExDiv": None,
-                    "Date_Paiement": None,
-                    "Dividende": float(val[-2]),
-                    "Status": 'No Change',
-                }
+        for idx in range(1, len(driver.find_elements(By.XPATH, '//*[@id="ec-reports-table"]/tbody/tr'))+1):
+            symbol: str = driver.find_element(
+                By.XPATH, f'//*[@id="ec-reports-table"]/tbody/tr[{idx}]/td[1]/a/div/span[1]').text
+            name: str = driver.find_element(
+                By.XPATH, f'//*[@id="ec-reports-table"]/tbody/tr[{idx}]/td[1]/a/div/span[2]').text
+            sector: str = driver.find_element(
+                By.XPATH, f'//*[@id="ec-reports-table"]/tbody/tr[{idx}]/td[2]/span[2]').text
+            dateExDividendeData: str = driver.find_element(
+                By.XPATH, f'//*[@id="ec-reports-table"]/tbody/tr[{idx}]/td[3]/span[2]').text
+            datePaymentData: str = driver.find_element(
+                By.XPATH, f'//*[@id="ec-reports-table"]/tbody/tr[{idx}]/td[4]/span[2]').text
+            dividendePerShareData: str = driver.find_element(
+                By.XPATH, f'//*[@id="ec-reports-table"]/tbody/tr[{idx}]/td[6]/span[2]').text
 
-                if (val[-5] == '-' or not search(r"\d{4}-\d{2}-\d{2}", val[-5])):
-                    data["Date_ExDiv"] = ''
-                else:
-                    dateExDiv = val[-5].split("-")
-                    data["Date_ExDiv"] = datetime(
+            if (symbol != ""):
+                dateExDividende: datetime = datetime.min
+                datePayment: datetime = datetime.min
+                dividendePerShare: float = .0
+
+                if (search(r"\d{4}-\d{2}-\d{2}", dateExDividendeData)):
+                    dateExDiv: list[str] = dateExDividendeData.split("-")
+                    dateExDividende = datetime(
                         int(dateExDiv[0]), int(dateExDiv[1]), int(dateExDiv[2]))
 
-                if (val[-4] == '-' or not search(r"\d{4}-\d{2}-\d{2}", val[-4])):
-                    data["Date_Paiement"] = ''
+                if (search(r"\d{4}-\d{2}-\d{2}", datePaymentData)):
+                    datePay: list[str] = datePaymentData.split("-")
+                    datePayment = datetime(
+                        int(datePay[0]), int(datePay[1]), int(datePay[2]))
+
+                if (dividendePerShareData != ''):
+                    dividendePerShare = float(dividendePerShareData)
+
+                findBySymbolStock: Stock | None = await Stock.prisma().find_unique(
+                    where={
+                        'symbol': symbol
+                    }
+                )
+                findByAllDataStock: Stock | None = await Stock.prisma().find_first(
+                    where={
+                        'symbol': symbol,
+                        'name': name,
+                        'sector': sector,
+                    }
+                )
+
+                if (findBySymbolStock == None and findByAllDataStock == None):
+                    await Stock.prisma().create(
+                        data={
+                            'symbol': symbol,
+                            'name': name,
+                            'sector': sector,
+                            'price': 0.0,
+                            'status': Status.NEW,
+                        }
+                    )
+                elif (findBySymbolStock != None and findByAllDataStock == None):
+                    await Stock.prisma().update(
+                        where={
+                            'symbol': symbol
+                        },
+                        data={
+                            'name': name,
+                            'sector': sector,
+                            'status': Status.UPDATED,
+                        }
+                    )
                 else:
-                    datePaiement = val[-4].split("-")
-                    data["Date_Paiement"] = datetime(
-                        int(datePaiement[0]), int(datePaiement[1]), int(datePaiement[2]))
+                    await Stock.prisma().update(
+                        where={
+                            'symbol': symbol
+                        },
+                        data={
+                            'status': Status.NOT_CHANGED,
+                        }
+                    )
+                
+                findBySymbolDividende: Dividende | None = await db.dividende.find_unique(
+                    where={
+                        'stockSymbol': symbol
+                    }
+                )
+                findByAllDataDividende: Dividende | None = await db.dividende.find_first(
+                    where={
+                        'stockSymbol': symbol,
+                        'dateExDividende': dateExDividende,
+                        'datePayment': datePayment,
+                        'dividendePerShare': dividendePerShare,
+                    }
+                )
 
-                # ------------------ Rework ------------------
-                dataReq = data.copy()
-                del dataReq['Status']
-
-                if (dividende.find_one(dataReq) != None):
-                    data["Status"] = 'Update'
-
-                    if (dividende.find_one(data) != None):
-                        data["Status"] = 'No Change'
-                        dividende.find_one_and_replace(
-                            {"Symbol": val[0]}, data)
-
-                    data["Status"] = "New"
-
-                    if (dividende.find_one(data) != None):
-                        data["Status"] = 'No Change'
-                        dividende.find_one_and_replace(
-                            {"Symbol": val[0]}, data)
+                if (findBySymbolDividende == None and findByAllDataDividende == None):
+                    await Dividende.prisma().create(
+                        data={
+                            'stockSymbol': symbol,
+                            'dateExDividende': dateExDividende,
+                            'datePayment': datePayment,
+                            'dividendePerShare': dividendePerShare,
+                            'status': Status.NEW
+                        }
+                    )
+                elif (findBySymbolDividende != None and findByAllDataDividende == None):
+                    await Dividende.prisma().update(
+                        where={
+                            'stockSymbol': symbol,
+                        },
+                        data={
+                            'dateExDividende': dateExDividende,
+                            'datePayment': datePayment,
+                            'dividendePerShare': dividendePerShare,
+                            'status': Status.NEW
+                        }
+                    )
                 else:
-                    if (dividende.find_one({ 'Symbol': val[0] })):
-                        data["Status"] = 'Update'
-                        dividende.find_one_and_replace(
-                            {"Symbol": val[0]}, data)
-                    else:
-                        data["Status"] = 'New'
-                        dividende.insert_one(data)
+                    await Dividende.prisma().update(
+                        where={
+                            'stockSymbol': symbol
+                        },
+                            data={
+                            'status': Status.NOT_CHANGED,
+                        }
+                    )
 
+    await db.disconnect()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
